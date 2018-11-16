38295be124a2ab1a4f3b7ad8ae277cc55ac5e7e3
Mark Philipp <mphilipp622@gmail.com>
2018-11-15 16:12:52 -0800

    Benchmark Tools

---
+++ BlockingQueue.cpp
@@ -0,0 +1,115 @@
+#include "BlockingQueue.h"
+
+BlockingQueue::BlockingQueue()
+{
+	front = nextEmpty = 0;
+	
+	// initialize the vector with 0's.	
+	for(int i = 0; i < maxItems; i++)
+		items.push_back(0);
+}
+
+BlockingQueue::BlockingQueue(int consumerInterval, int producerInterval) : BlockingQueue()
+{
+	output.open("data/" + to_string(maxItems) + "_" + to_string(producerInterval) + "_" + to_string(consumerInterval) + ".csv");
+	output << "capacity,timestamp,countFull,countEmpty\n";
+
+	startTime = chrono::system_clock::now();
+}
+
+BlockingQueue::~BlockingQueue()
+{
+	output.close();	
+}
+
+void BlockingQueue::Insert(int item, int threadID)
+{
+	// acquire lock
+	unique_lock<mutex> uniqueLock(lock);
+	
+	// If queue is full, have producer wait for consumer to remove something
+	while((nextEmpty - front) == maxItems)
+	{
+		countFull++;
+		string message = "Producer " + to_string(threadID) + " Waiting to Produce\n\n";
+		cout << message;
+
+		itemRemoved.wait(uniqueLock);
+	}
+	
+	// Add the item to the queue
+	items.at(nextEmpty % maxItems) = item;
+	
+	// Increment next index
+	nextEmpty++;
+
+	WriteDataToCSV();
+
+	string message = to_string(item) + " produced by producer " + to_string(threadID) + "\n\n";
+		
+	cout << message; // print success message
+	
+	// release lock
+	uniqueLock.unlock();
+
+	// According to C++ std::condition_variable documentation:
+	// Manual unlocking is done before notifying, to avoid waking up
+    // the waiting thread only to block again
+
+	// Notify waiting consumers
+	itemAdded.notify_one();
+}
+
+void BlockingQueue::Remove(int threadID)
+{
+	int item = 0; // this will be given a value and returned
+	
+	// Acquire lock
+	unique_lock<mutex> uniqueLock(lock);
+	
+	// If there is nothing in the queue, tell consumer to wait for producer to add something
+	while(front == nextEmpty)
+	{
+		countEmpty++;
+		string message = "Consumer " + to_string(threadID) + " Waiting to Consume\n\n";
+		cout << message;
+
+		itemAdded.wait(uniqueLock);
+	}
+	
+	// Get item from queue
+	item = items.at(front % maxItems);
+
+	// free up a spot in the queue
+	front++;
+
+	WriteDataToCSV();
+
+	string message = to_string(item) + " consumed by consumer " + to_string(threadID) + "\n\n";
+	cout << message; // output success message
+
+	// release lock
+	uniqueLock.unlock();
+
+	// According to C++ std::condition_variable documentation:
+	// Manual unlocking is done before notifying, to avoid waking up
+    // the waiting thread only to block again
+
+	// Notify any waiting producers that there's a space free in the queue.
+	itemRemoved.notify_one();
+	
+	// Return the item
+}
+
+float BlockingQueue::GetPercentageFull()
+{
+    return (nextEmpty - front) / (float) maxItems; // returns how full the queue is as a percentage
+}
+
+void BlockingQueue::WriteDataToCSV()
+{
+	auto end = chrono::system_clock::now();
+	auto elapsed = end - startTime;
+
+	output << to_string(GetPercentageFull() * 100) + "," + to_string(elapsed.count()) + "," + to_string(countFull) + "," + to_string(countEmpty) + "\n";
+}
---
+++ main.cpp
@@ -0,0 +1,54 @@
+/*
+Mark Philipp
+CSCI 144
+10/19/18
+*/
+
+#include <iostream>
+#include <ctime> // used for timer
+#include <cstdlib>
+#include <string>
+#include "Producer.h"
+#include "Consumer.h"
+#include "BoundedQueue.h"
+#include "BlockingQueue.h"
+
+int main(int argc, char *argv[])
+{
+	int producerInterval = 0, consumerInterval = 0;
+
+	srand(time(NULL));
+
+	cout << "PROGRAM START" << endl;
+
+	if(argc > 2)
+	{
+		// Assign intervals from command line arguments
+		producerInterval = stoi(argv[1]);
+		consumerInterval = stoi(argv[2]);
+	}
+	else
+	{
+		// Throw an error and exit program if program is run without parameters
+		cout << "Command Line Argument Error: Program requires producer interval and consumer interval be set" << endl;
+		return 0;
+	}
+
+	BlockingQueue* queue = new BlockingQueue(consumerInterval, producerInterval);
+
+	Consumer* consumers[10];
+	Producer* producers[10];
+
+	for(int i = 0; i < 10; i++)
+	{
+		// initialize all the consumer and producer threads. ID's are i + 1 (1 - 10)
+		consumers[i] = new Consumer(queue, i + 1, consumerInterval);
+		producers[i] = new Producer(queue, i + 1, producerInterval);
+	}
+	
+	// Waiting for a single join() will keep execution running forever.
+	consumers[0]->JoinThread();
+	producers[0]->JoinThread();
+
+	return 0;
+}
